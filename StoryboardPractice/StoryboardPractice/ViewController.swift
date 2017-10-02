49c65b36adb0fb4d1980f76e3cfdc593d9520d33
Bang Chiang Liao <cat1357913579@hotmail.com>
2017-10-02 21:42:39 +0800

    Project init.

---
+++ StoryboardPractice/StoryboardPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardPractice
+//
+//  Created by Bang Chiang Liao on 02/10/2017.
+//  Copyright © 2017 Bang Chiang Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
